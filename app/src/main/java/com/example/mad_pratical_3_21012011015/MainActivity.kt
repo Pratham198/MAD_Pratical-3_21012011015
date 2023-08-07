0e77641752e4c1d496b6559228576458119aad0f
MY GPSC <danipratham4581@gmail.com>
2023-08-07 07:36:19 +0530

    Practical-3

---
+++ app/src/main/java/com/example/mad_pratical_3_21012011015/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mad_pratical_3_21012011015
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
